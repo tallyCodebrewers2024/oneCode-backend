d59826579c48ad9ccfc12c9faa3b6477551fe731
kalagavamsidhar <vamsidharkalaga@kgpian.iitkgp.ac.in>
2024-08-09 19:38:48 +0530

    delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.listen(8800, () => {
-  console.log("Listening on Port 8800!");
-});
