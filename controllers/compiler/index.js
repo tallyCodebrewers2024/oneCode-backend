3cc11512987892b19a232af5b4ea7dd525b042be
kalagavamsidhar <vamsidharkalaga@kgpian.iitkgp.ac.in>
2024-08-10 15:28:17 +0530

    fix merge conflicts

--- controllers/compiler/index.js
+++ controllers/compiler/index.js
@@ -48,8 +48,6 @@ exports.runcode = async (req, res) => {
 
     let container;
     try {
-        // Record the start time
-        
         // Create Docker container
         container = await docker.createContainer({
             Image: image,
@@ -64,23 +62,10 @@ exports.runcode = async (req, res) => {
                 ? ["sh", "-c", `${compileCmd} && ${runCmd}`]
                 : runCmd.split(" "),
         });
-        
+
         // Start the container
         await container.start();
-        
-        // const containerInfo = await container.inspect();
-        // const startTime = containerInfo.State.StartedAt;
 
-        // let isRunning = false;
-        // while (!isRunning) {
-        //     const containerInfo = await container.inspect();
-        //     isRunning = containerInfo.State.Running
-        //     await new Promise((resolve) => setTimeout(resolve, 50)); // Wait for a bit before checking again
-        // }
-        
-        // const startTime = process.hrtime();
-
-        
         // Capture output with stream handling
         let output = "";
         output = await new Promise((resolve, reject) => {
@@ -91,27 +76,26 @@ exports.runcode = async (req, res) => {
                         return reject(err);
                     }
                     let result = "";
-                    
+
                     stream.on("data", (chunk) => {
-                        result += chunk.toString("utf8"); // Convert buffer to UTF-8 string
+                        result += chunk.toString("utf8"); 
                     });
-                    
+
                     stream.on("end", () => {
                         result = result.replace(
                             /[\u0000-\u001F\u007F-\u009F]/g,
                             ""
-                        ); // Clean the output
+                        );
                         resolve(result);
                     });
-                    
+
                     stream.on("error", (err) => {
                         reject(err);
                     });
                 }
             );
         });
-        // const endTime = process.hrtime(startTime);
-        
+
         let error = "";
         error = await new Promise((resolve, reject) => {
             container.logs(
@@ -121,19 +105,19 @@ exports.runcode = async (req, res) => {
                         return reject(err);
                     }
                     let result = "";
-                    
+
                     stream.on("data", (chunk) => {
-                        result += chunk.toString("utf8"); // Convert buffer to UTF-8 string
+                        result += chunk.toString("utf8"); 
                     });
-                    
+
                     stream.on("end", () => {
                         result = result.replace(
                             /[\u0000-\u001F\u007F-\u009F]/g,
                             ""
-                        ); // Clean the output
+                        ); 
                         resolve(result);
                     });
-                    
+
                     stream.on("error", (err) => {
                         reject(err);
                     });
@@ -144,18 +128,12 @@ exports.runcode = async (req, res) => {
         const containerInfo = await container.inspect();
         const startTime = containerInfo.State.StartedAt;
         const endTime = containerInfo.State.FinishedAt;
-        console.log("Start Time: ", startTime);
-        console.log("End Time: ", endTime);
 
         const runtime = new Date(endTime) - new Date(startTime);
 
         // Wait for the container to finish
         await container.wait();
         await container.remove();
-        // Record the end time
-        
-        // Convert the runtime to seconds and milliseconds
-        // const runtime = (endTime[0] + endTime[1] / 1e9).toFixed(6) + " seconds";
 
         res.status(201).json({ output, error, runtime });
     } catch (error) {
@@ -166,7 +144,7 @@ exports.runcode = async (req, res) => {
     } finally {
         try {
             if (fs.existsSync(filePath)) {
-                fs.unlinkSync(filePath); // Ensure the file is deleted even if an error occurs
+                fs.unlinkSync(filePath);
                 console.log(`Deleted file: ${filePath}`);
             } else {
                 console.log(`File not found: ${filePath}`);
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { requireSession } = require("@clerk/clerk-sdk-node");
 const User = require("./models/user");
 const { connectToMongoDB } = require("./db.js");
 
@@ -8,26 +7,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api", require("./routes/index"));
-
-app.post("/webhook/clerk", async (req, res) => {
-  try {
-    await connectToMongoDB();
-    const event = req.body;
-
-    if (event.type === "user.created") {
-      const { id, email } = event.data;
-
-      // Save user to MongoDB
-      const user = new User({ clerkUserId: id, email });
-      await user.save();
-    }
-
-    res.status(200).send("OK");
-  } catch (error) {
-    console.error("Webhook error:", error);
-    res.status(500).send("Error");
-  }
-});
 
 app.listen(5050, () => {
   console.log("Listening on Port 5050!");
