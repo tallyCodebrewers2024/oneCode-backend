cb7a09d5dbfa97b7fb442bdf5aabb6914fc302b6
Nikhil <nikhilrajani309@gmail.com>
2024-08-11 01:33:28 +0530

    Changes in add problem

--- controllers/problems/index.js
+++ controllers/problems/index.js
@@ -1,29 +1,34 @@
 const Problem = require("../../models/Problem");
+const { v4: uuidv4 } = require("uuid");
 
 exports.addProblem = async (req, res) => {
-	const problemData = req.body;
+  const problemData = req.body;
 
-	if (problemData === undefined) {
-		return res.status(401).json({ message: "Problem Not Found!" });
-	}
+  if (problemData === undefined) {
+    return res.status(401).json({ message: "Problem Not Found!" });
+  }
 
-	try {
-		await Problem.create(problemData);
+  const user = req.user;
+  const author = user.id;
+  const problemId = uuidv4();
 
-		return res.status(201).json({ message: "Problem Added Succesfully!" });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ error: "Error adding problem" });
-	}
+  try {
+    await Problem.create({ problemId, ...problemData, author });
+
+    return res.status(201).json({ message: "Problem Added Succesfully!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error adding problem" });
+  }
 };
 
 exports.getProblems = async (req, res) => {
-	try {
-		const problems = await Problem.find();
+  try {
+    const problems = await Problem.find();
 
-		return res.status(200).json(problems);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ error: "Error fetching problems" });
-	}
+    return res.status(200).json(problems);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching problems" });
+  }
 };
